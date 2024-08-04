354d5bbe50aa4bf7612ecebe76a20b83e9cf69ad
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2024-08-04 10:30:52 -0500

    Move echo templ render to it's own file

--- web/chat.go
+++ web/chat.go
@@ -3,22 +3,10 @@ package web
 import (
 	"net/http"
 
-	"github.com/a-h/templ"
 	"github.com/cole-maxwell1/chatroom/web/templates"
 	"github.com/labstack/echo/v4"
 )
 
 func RenderChatRoom(c echo.Context, broker *WebSocketBroker) error {
 	return Render(c, http.StatusOK, templates.ChatDisplay(chatMessages.Get(), len(broker.clients)))
-}
-
-func Render(ctx echo.Context, statusCode int, t templ.Component) error {
-	buf := templ.GetBuffer()
-	defer templ.ReleaseBuffer(buf)
-
-	if err := t.Render(ctx.Request().Context(), buf); err != nil {
-		return err
-	}
-
-	return ctx.HTML(statusCode, buf.String())
 }
